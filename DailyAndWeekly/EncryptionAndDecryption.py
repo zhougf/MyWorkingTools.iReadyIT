b2d3db605e1b907fbd9d24d660433a0aad944472
Zhou Micheal <541430660@qq.com>
2020-04-25 15:33:41 +0800

    Change AES encryption mode from MODE_CFB to MODE_ECB.

--- DailyAndWeekly/EncryptionAndDecryption.py
+++ DailyAndWeekly/EncryptionAndDecryption.py
@@ -17,14 +17,10 @@ from Crypto.Cipher import AES
 
 
 class EncryptionAndDecryption(object):
-    def __init__(self, key, iv=None, mode=AES.MODE_CFB):
+    def __init__(self, key, mode=AES.MODE_ECB):
+        # MODE_ECB为加密模式
         self.key = self.PadKey(key)
-        self.mode = mode  # MODE_CFB为加密模式
-        if iv:
-            self.iv = iv
-        else:
-            self.iv = Random.new().read(AES.block_size)
-            # self.iv = Random.new().read(16)  # 随机向量，必须是16字节长度
+        self.mode = mode
 
     # 加密函数，如果text不是16的倍数【加密文本text必须为16的倍数！】，那就补足为16的倍数
     # 加密内容需要长达16位字符，所以进行空格拼接
@@ -41,26 +37,25 @@ class EncryptionAndDecryption(object):
 
     def Encryption(self, text):
         # 这里密钥key 长度必须为16（AES-128）、24（AES-192）、或32（AES-256）Bytes 长度.目前AES-128足够用
-        # 加密的字符需要转换为bytes
         data = self.PadText(text)
-        aes = AES.new(self.key, self.mode, self.iv)  # 密文生成器,MODE_CFB为加密模式
-        result = self.iv + aes.encrypt(data)  # 附加上iv值是为了在解密时找到在加密时用到的随机iv
-        # 因为AES加密时候得到的字符串不一定是ascii字符集的，输出到终端或者保存时候可能存在问题
-        # 所以这里统一把加密后的字符串转化为16进制字符串
-        return binascii.b2a_hex(result)  # 将二进制密文转换为16机制显示
+        # 加密的字符需要转换为bytes
+        aes = AES.new(self.key, self.mode)
+        # 密文生成器,MODE_ECB为加密模式
+        result = aes.encrypt(data)
+        return binascii.b2a_hex(result)
+        # 将二进制密文转换为16机制显示
 
     # 解密后，去掉补足的空格用strip() 去掉
     def Decryption(self, encrypt_msg):
-        text = binascii.a2b_hex(encrypt_msg)
-        aes = AES.new(self.key, self.mode, self.iv)  # 解密时必须重新创建新的密文生成器
-        return aes.decrypt(text[AES.block_size:]).rstrip(' ')
+        aes = AES.new(self.key, self.mode)
+        # 解密时必须重新创建新的密文生成器
+        return aes.decrypt(binascii.a2b_hex(encrypt_msg)).rstrip(' ')
 
 
 if __name__ == "__main__":
-    k = b"iReadyITiReadyIT"
+    k = b"iReadyIT"
     pc = EncryptionAndDecryption(k)  # 初始化密钥
-    iv = pc.iv
-    data = ('我是明文测试')
+    data = ('这里是测试，我是明文。')
     e = pc.Encryption(data)
     d = pc.Decryption(e)
     print "加密Key是：" + k, "\n明文是：" + d, "\n密文是：" + e
