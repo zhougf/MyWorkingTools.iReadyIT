9dbf147e6b35a07cdaba7cf4642a00c98b07efe9
Zhou Micheal <541430660@qq.com>
2020-04-25 16:18:01 +0800

    Change AES encryption mode to MODE_CFB again.

--- DailyAndWeekly/EncryptionAndDecryption.py
+++ DailyAndWeekly/EncryptionAndDecryption.py
@@ -12,15 +12,24 @@
 """
 
 import binascii
-from Crypto import Random
+# from Crypto import Random
 from Crypto.Cipher import AES
 
 
-class EncryptionAndDecryption(object):
-    def __init__(self, key, mode=AES.MODE_ECB):
-        # MODE_ECB为加密模式
+class EncryptionAndDecryption:
+    def __init__(self, key, iv, mode=AES.MODE_CFB):
+        # MODE_CFB为加密模式
         self.key = self.PadKey(key)
         self.mode = mode
+        self.iv = iv
+        """
+        if iv:
+            self.iv = iv
+        else:
+            self.iv = '1234567890123456'
+            # self.iv = Random.new().read(AES.block_size)
+            # self.iv = Random.new().read(16)  # 随机向量，必须是16字节长度
+        """
 
     # 加密函数，如果text不是16的倍数【加密文本text必须为16的倍数！】，那就补足为16的倍数
     # 加密内容需要长达16位字符，所以进行空格拼接
@@ -37,25 +46,27 @@ class EncryptionAndDecryption(object):
 
     def Encryption(self, text):
         # 这里密钥key 长度必须为16（AES-128）、24（AES-192）、或32（AES-256）Bytes 长度.目前AES-128足够用
-        data = self.PadText(text)
         # 加密的字符需要转换为bytes
-        aes = AES.new(self.key, self.mode)
-        # 密文生成器,MODE_ECB为加密模式
-        result = aes.encrypt(data)
-        return binascii.b2a_hex(result)
-        # 将二进制密文转换为16机制显示
+        aes = AES.new(self.key, self.mode, self.iv)
+        # 密文生成器,MODE_CFB为加密模式
+        result = self.iv + aes.encrypt(self.PadText(text))
+        # 附加上iv值是为了在解密时找到在加密时用到的iv
+        return binascii.b2a_hex(result)  # 将二进制密文转换为16机制显示
 
     # 解密后，去掉补足的空格用strip() 去掉
     def Decryption(self, encrypt_msg):
-        aes = AES.new(self.key, self.mode)
+        aes = AES.new(self.key, self.mode, self.iv)
         # 解密时必须重新创建新的密文生成器
-        return aes.decrypt(binascii.a2b_hex(encrypt_msg)).rstrip(' ')
+        return aes.decrypt(binascii.a2b_hex(encrypt_msg)[AES.block_size:]).rstrip(' ')
 
 
 if __name__ == "__main__":
-    k = b"iReadyIT"
-    pc = EncryptionAndDecryption(k)  # 初始化密钥
-    data = ('这里是测试，我是明文。')
+    key = b"iReadyIT"
+    iv = '1234567890123456'
+    # 指定的向量，必须是16字节长度
+    pc = EncryptionAndDecryption(key, iv)
+    # 初始化密钥
+    data = ('测试一下，我是明文。')
     e = pc.Encryption(data)
     d = pc.Decryption(e)
-    print "加密Key是：" + k, "\n明文是：" + d, "\n密文是：" + e
+    print "加密Key是：" + key, "\n明文是：" + d, "\n密文是：" + e
